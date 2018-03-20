9c95833711d812a1db444a16340a9bf49609e525
Sebastian Real <sreal@falabella.cl>
2018-03-20 14:57:00 -0300

    added filter util to compose conditions and isolate reset results

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,10 @@ const BASE_URL = 'https://pixabay.com/api?key=7449813-3f6b2a4c78c08db391996117d'
 // first functional utility, it creates another function
 // which will take the property from the object and return it
 const prop = property => x => x[property];
+// second functional utility, it creates another function
+// which takes a predicate function to remove every element which does not
+// match the current condition
+const filter = fn => xs => xs.filter(fn);
 
 const fetchByTerm = (searchTerm) => {
   return $.get(`${BASE_URL}&q=${encodeURI(searchTerm)}`);
@@ -34,41 +38,56 @@ const impureDOM = (() => {
   };
 })();
 
+const impureResults = (() => {
+  const $results = $('#results');
+  const $tip = $('#tip');
+
+  return {
+    resetResults: ({ searchTerm, likes, minHeight, minWidth }) => {
+      $results.empty();
+      $tip.html(`Your results for <strong>${searchTerm}</strong> -> width: ${minWidth}px, height: ${minHeight}px, likes: ${likes} `);
+    }
+  };
+})();
+
 $('#submit').on('click', () => {
   const { searchTerm, likes, minHeight, minWidth } = impureDOM.read();
 
   fetchByTerm(searchTerm)
   .then(prop('hits'))
+  .then(filter(hit => hit.likes >= likes))
+  .then(filter(hit => hit.webformatHeight >= minHeight))
+  .then(filter(hit => hit.webformatWidth >= minWidth))
+  .then(hits => {
+    impureResults.resetResults(impureDOM.read());
+    return hits;
+  })
   .then((hits) => {
-    $('#results').empty();
     console.log('response', hits);
-
-    $('#tip').html(`Your results for <strong>${searchTerm}</strong> -> width: ${minWidth}px, height: ${minHeight}px, likes: ${likes} `);
 
     for(let i = 0; i < hits.length; i++) {
       const hit = hits[i];
 
-      if (hit.likes >= likes && hit.webformatHeight >= minHeight && hit.webformatWidth >= minWidth) {
-        const image = hit.webformatURL;
-        const tag = hit.tags;
-        const likes = hit.likes;
-        const height = hit.webformatHeight;
-        const width = hit.webformatWidth;
+      const image = hit.webformatURL;
+      const tag = hit.tags;
+      const likes = hit.likes;
+      const height = hit.webformatHeight;
+      const width = hit.webformatWidth;
 
-        $('#results').append(`
-          <div class="card">
-            <img class="card__image" src="${image}" alt="Card image cap">
-            <div class="card__body">
-              <p class="card__title">${tag}</p>
-              <p class="card__likes">${likes}</p>
-              <p class="card__width">${width}</p>
-              <p class="card__height">${height}</p>
-            </div>
+      $('#results').append(`
+        <div class="card">
+          <img class="card__image" src="${image}" alt="Card image cap">
+          <div class="card__body">
+            <p class="card__title">${tag}</p>
+            <p class="card__likes">${likes}</p>
+            <p class="card__width">${width}</p>
+            <p class="card__height">${height}</p>
           </div>
-        `);
-      }
+        </div>
+      `);
     }
-
+  })
+  .then(() => {
     impureDOM.reset();
   });
 });
